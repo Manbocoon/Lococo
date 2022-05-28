98c39d672f404f87cdc790fe466979b077297631
bumju08 <debugprov@naver.com>
2022-05-28 15:49:14 +0900

    Configurator

---
+++ Config/_public.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lococo.Config
+{
+
+    public static class _public
+    {
+
+        public static void CreateDirectories()
+        {
+            Directory.CreateDirectory(Program.Path + "\\db");
+            Directory.CreateDirectory(Program.Path + "\\db\\settings");
+            Directory.CreateDirectory(Program.Path + "\\db\\settings\\favorites");
+        }
+
+    }
+}
