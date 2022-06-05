1f0164351ef2a5557d77f2a5d9720093d5723a40
bumju08 <debugprov@naver.com>
2022-06-05 20:13:25 +0900

    Functions for imaging

--- Functions/Imaging.cs
+++ Functions/Imaging.cs
@@ -11,8 +11,15 @@ using System.Windows.Media.Imaging;
 
 namespace Lococo
 {
+    /// <summary>
+    /// 이미지 처리에 사용될 함수들을 모아놓은 정적 객체입니다.
+    /// </summary>
     static class Imaging
     {
+
+        /// <summary>
+        /// 특정 경로의 파일이 올바른 이미지인지 확인합니다.
+        /// </summary>
         public static bool IsSupportedImage(string file_path)
         {
             if (!File.Exists(file_path))
@@ -40,6 +47,9 @@ namespace Lococo
             return result;
         }
 
+        /// <summary>
+        /// 특정 경로의 이미지파일의 넓이를 구합니다.
+        /// </summary>
         public static Size GetImageSize(string file_path)
         {
             if (!File.Exists(file_path))
@@ -62,6 +72,10 @@ namespace Lococo
             return result;
         }
 
+        /// <summary>
+        /// 넓이를 통해 이미지의 가로:세로 비율을 구하고, Point 변수를 통해 X:Y로 반환합니다.
+        /// </summary>
+        /// <param name="image">비율을 추출해낼 이미지의 넓이입니다.</param>
         public static Point GetImageRatio(Size image)
         {
             int gcd = _Math.GetGCD(image.Width, image.Height);
